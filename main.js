41ddf649480105a162aa005fc2367e790186cbc6
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-04 15:35:32 -0800

    Use Bootstrap and keep a list of currently logged-in users

--- main.js
+++ main.js
@@ -3,44 +3,82 @@
 // connect to firebase
 var ref = new Firebase('https://nicholasapp.firebaseio.com/');
 var chatRef = ref.child('chat'); // separate directory in the above firebase
+var usersRef = ref.child('users');
 
 $(document).ready(() => {
 
   let $name = $('#name');
   let $msg = $('#msg');
   let $add = $('#add');
+  let $users = $('#userlist');
+  let $log = $('.log');
   let $conversation = $('#conversation');
   var name;
 
   // event listeners
   $('#start').click(start);
   $add.click(addMessage);
-  $('#name').on('keypress', (e) => {
+  $name.on('keypress', (e) => {
     if (e.charCode === 13) start();
   }).focus();
-  $('#msg').on('keypress', (e) => {
+  $msg.on('keypress', (e) => {
     if (e.charCode === 13) addMessage();
   }).focus();
+  $('#logout').click(logout);
 
-  // firebase listener
+  // firebase listeners
   chatRef.on('child_added', (snapshot) => {
     var value = snapshot.val();
     var $li = $('<li>').text(value.time + ' ' + value.name + ': ' + value.message);
     $conversation.append($li);
   });
+  usersRef.on('value', (snapshot) => {
+    var users = snapshot.val();
+
+    var $lis = [];
+
+    for (var key in users) {
+      var $li = $('<li>').text(users[key]);
+      $lis.push($li);
+    }
+
+    $users.empty().append($lis);
+  })
 
   function start() {
     name = $name.val();
     $name.val('');
     // name validation
     if (name.length > 0 && name.length < 15) {
+      $log.show();
       $('.newMessage').show();
+      $('#logout').show()
       $('.chooseName').hide();
+      usersRef.push(name);
       $msg.focus();
-      $add.after( $('<p>').text('logged in as ' + name).css('font-style', 'italic') );
+      $('#username').text('logged in as ' + name).css('font-style', 'italic');
     } else {
-      $name.val('too long!')
+      $name.val('too long!');
     }
+  }
+
+  function logout() {
+    // same as on, but only triggers once
+    usersRef.once('value', (snapshot) => {
+      // console.log(snapshot.val());
+      let users = snapshot.val();
+      for (let key in users) {
+        if (users[key] === name) {
+          // remove from list
+          usersRef.child(key).remove();
+        }
+      }
+      $log.hide();
+      $('.newMessage').hide();
+      $('#logout').hide()
+      $('.chooseName').show();
+
+    }) 
   }
 
   function addMessage() {
